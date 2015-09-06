b3faae3820710aa678c7b9109df97baf127e0283
Erik Berglund <erikberglund@Eriks-MacBook-Pro.local>
2015-09-06 13:07:22 +0200

    Added IP validation Category

---
+++ NBICreator/NBICreatorCategories/NSString+validIP.h
@@ -0,0 +1,14 @@
+//
+//  NSString+validIP.h
+//  NBICreator
+//
+//  Taken from http://stackoverflow.com/a/10971521/4596429.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (NBCvalidIP)
+
+- (BOOL)isValidIPAddress;
+
+@end
